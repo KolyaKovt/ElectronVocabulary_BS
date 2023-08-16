deaab4898307d7e11322de5bbaa95295a6ff4b24
KovtunetsMykola <109476933+Kovtunets-ProgrammerOfHisLife@users.noreply.github.com>
2023-08-16 09:40:28 +0200

    fixed the delete notification

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,40 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const Question = ({ action }) => {
-  const [toastShown, setToastShown] = useState(false)
-
   const handleContinueClick = () => {
-    if (!toastShown) {
-      const toastId = toast.info('Do you want to continue?', {
-        position: toast.POSITION.TOP_CENTER,
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        closeButton: (
-          <div>
-            <button onClick={() => handleConfirm(toastId)}>Yes</button>
-            <button onClick={() => handleCancel(toastId)}>No</button>
-          </div>
-        ),
-      })
-      setToastShown(true)
-    }
+    const toastId = toast.info('Do you want to continue?', {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      closeButton: (
+        <div>
+          <button onClick={() => handleConfirm(toastId)}>Yes</button>
+          <button onClick={() => handleCancel(toastId)}>No</button>
+        </div>
+      ),
+    })
   }
 
   const handleConfirm = toastId => {
     toast.dismiss(toastId)
     action()
-    setToastShown(false)
   }
 
   const handleCancel = toastId => {
     toast.dismiss(toastId)
-    setToastShown(false)
   }
 
   return (
